53b0c3cf4e68e5f7988b8c377229a3fe98ef40b5
MurcyLetch <arijitindu99@gmail.com>
2021-12-11 13:40:28 +0530

    time_react_hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 const App = () =>{
-    const [count,setCount]=useState(0);
-    const Incnum =()=>{
-        setCount(count+1);
-    };
+    //const [count,setCount]=useState(0);
+    //const Incnum =()=>{
+      //  setCount(count+1);
+    //};
+    let time=new Date().toLocaleTimeString();
+    const [ctime,setctime]=useState(time);
+    const solve =()=>{
+        time=new Date().toLocaleTimeString();
+        setctime(time);
+    }
     return (
         <>
-            <h1 className="heading_style">{count}</h1>
-            <button className="button_style" onClick={Incnum}>Click Me</button>
+            <h1 className="heading_style">{ctime}</h1>
+            <button className="button_style" onClick={solve}>Get Time</button>
         </>
     )
 }
